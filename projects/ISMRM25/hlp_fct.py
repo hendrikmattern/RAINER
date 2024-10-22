0782251f65be80c144d9a40cdae1984425252363
hmattern <hendrik.mattern@ovgu.de>
2024-10-22 13:52:25 +0200

    major update: created inital package

--- projects/ISMRM25/hlp_fct.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def get_data_path(work_locally=False):
-    if work_locally:
-        data_path = "C:\\Users\\Hendrik Mattern\\Downloads\\ismrm25_nii"
-    else:
-        data_path = "Y:\\Projects\\ISMRM25_EPI_MREG\\nii"
-    return data_path
